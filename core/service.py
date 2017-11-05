44211c3d03ef268dbaa93f47603ee59bc06d4a1b
guilherme <guilhermegga@gmail.com>
2017-11-05 20:11:30 -0200

    alterando modo de busca na consulta

--- core/service.py
+++ core/service.py
@@ -11,10 +11,10 @@ from datetime import date
 
 cliente = MongoClient('localhost',27017)
 banco = cliente['local']
-db_pi = banco['novobanco']
+db_pi = banco['pi2']
 
 def sentimentos(palavra):
-    dados = db_pi.find({"$and":[{"text":{"$regex": u"{0}".format(palavra)}},{"lang":{"$eq":"en"}},{"createdAt":{"$gte":"Sep 19, 2017 2:22:08 PM","$lte" :"Sep 24, 2017 2:23:00 PM"}}]})
+    dados = db_pi.find({"$and":[{"text":{"$regex": u"{0}".format(palavra)}},{"lang":{"$eq":"en"}},{"createdAt":{"$gte":"Sep 14, 2017 2:22:08 PM","$lte" :"Sep 30, 2017 2:23:00 PM"}}]}).distinct("createdAt")
 
     # dados = db_pi.find({"$and" :[{"text":{"$regex": u"ladygaga"}},{"lang":{"$eq":"en"}}]}).limit(10)
     # valores=[]
